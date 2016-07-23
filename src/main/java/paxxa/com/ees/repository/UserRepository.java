c05e79ea3a403524c4635a02c3da04b49fa8ad96
SondaP <paxxa.kontakt@gmail.com>
2016-07-23 22:42:48 +0200

    Repo for user + repo for role

--- src/main/java/paxxa/com/ees/repository/UserRepository.java
+++ src/main/java/paxxa/com/ees/repository/UserRepository.java
@@ -3,5 +3,5 @@ package paxxa.com.ees.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import paxxa.com.ees.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> { 
 }
