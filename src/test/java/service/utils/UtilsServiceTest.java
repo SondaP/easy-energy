c6328e092dfda874f18e7559429bd56a5df389f6
SondaP <paxxa.kontakt@gmail.com>
2016-10-25 21:03:32 +0200

    Refactor

--- src/test/java/service/utils/UtilsServiceTest.java
+++ src/test/java/service/utils/UtilsServiceTest.java
@@ -54,7 +54,7 @@ public class UtilsServiceTest {
         //when
         Integer differenceDays = utilsService.countDaysBetweenTwoDates(startDate, endDate);
         //then
-        Assert.assertTrue(differenceDays == 26);
+        Assert.assertTrue(differenceDays == 25);
     }
 
     @Test
@@ -65,7 +65,7 @@ public class UtilsServiceTest {
         //when
         Integer differenceDays = utilsService.countDaysBetweenTwoDates(startDate, endDate);
         //then
-        Assert.assertTrue(differenceDays == 1);
+        Assert.assertTrue(differenceDays == 0);
     }
 
 }
